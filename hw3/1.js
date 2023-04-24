ee6164cdb3b636d3a244ce902f4c5981d02f7bbe
FoxyGinger <nirv4life@gmail.com>
2023-04-24 10:42:58 +0300

    Added hw3

---
+++ hw3/1.js
@@ -0,0 +1,4 @@
+const celsiusTemp = parseInt(prompt("Введите температуру в градусах Цельсия:"))
+const fahrenheitTemp = ((9 / 5) * celsiusTemp + 32).toFixed(1)
+
+alert(`Цельсий: ${celsiusTemp}, Фаренгейт: ${fahrenheitTemp}`)
