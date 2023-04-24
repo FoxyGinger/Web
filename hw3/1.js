2b84381f215e27c482a4440668d1f0a0999cf20a
FoxyGinger <nirv4life@gmail.com>
2023-04-24 12:26:10 +0300

    style fix: Number prefix for function parseInt

--- hw3/1.js
+++ hw3/1.js
@@ -1,4 +1,4 @@
-const celsiusTemp = parseInt(prompt("Введите температуру в градусах Цельсия:"))
+const celsiusTemp = Number.parseInt(prompt("Введите температуру в градусах Цельсия:"))
 const fahrenheitTemp = ((9 / 5) * celsiusTemp + 32).toFixed(1)
 
 alert(`Цельсий: ${celsiusTemp}, Фаренгейт: ${fahrenheitTemp}`)
