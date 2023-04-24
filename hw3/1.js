7bc7b59c3c74758aed9c095679f3e310af663aaf
FoxyGinger <nirv4life@gmail.com>
2023-04-24 13:39:29 +0300

    wrapped conversion to function

--- hw3/1.js
+++ hw3/1.js
@@ -1,4 +1,7 @@
 const celsiusTemp = Number.parseFloat(prompt("Введите температуру в градусах Цельсия:"))
-const fahrenheitTemp = ((9 / 5) * celsiusTemp + 32).toFixed(1)
 
-alert(`Цельсий: ${celsiusTemp}, Фаренгейт: ${fahrenheitTemp}`)
+function fahrenheitToCelsius(celsius){
+    return ((9 / 5) * celsius + 32).toFixed(1);
+}
+
+alert(`Цельсий: ${celsiusTemp}, Фаренгейт: ${fahrenheitToCelsius(celsiusTemp)}`)
